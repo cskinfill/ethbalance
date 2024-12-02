3f5afa69ed71f3dbb236bdd8e5782694a6092147
Craig Skinfill <223300+cskinfill@users.noreply.github.com>
2024-12-02 13:30:21 -0500

    adding transaction route

--- app.py
+++ app.py
@@ -51,6 +51,29 @@ def services(address, methods=["GET"]):
   else:
     abort(404, "Not Found")
 
+@app.route("/address/transaction/<address>")
+def transaction(address, methods=["GET"]):
+  request_id = uuid.uuid4().hex
+  payload = {
+    "jsonrpc": "2.0",
+    "method": "eth_getTransactionByHash",
+    "params": [
+      address
+    ],
+    "id": request_id
+  }
+  logger.debug(f"request payload is {payload}")
+  r = requests.post(f"https://mainnet.infura.io/v3/{API_KEY}", headers=HEADERS, json=payload)
+  logger.debug("Got back status code %s and body %s", r.status_code, r.text)
+  if r.status_code == 200:
+    resp = r.json()
+    if "error" in resp:
+      logger.warning(f"{resp["error"]}")
+      abort(404)
+    return jsonify(resp["result"])
+  else:
+    abort(404, "Not Found")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
